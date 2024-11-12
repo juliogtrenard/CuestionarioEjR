b2f2f6ab40b193bb25e353dca269e6d2e3ff6c49
Julio <juliogtrenard@gmail.com>
2024-11-12 11:56:16 +0100

    ADD dependencia para el Temporizador

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module es.juliogtrenard.cuestionarioejr {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires es.juliogtrenard.componentetemporizador;
 
     opens es.juliogtrenard.cuestionarioejr to javafx.fxml;
     exports es.juliogtrenard.cuestionarioejr;
